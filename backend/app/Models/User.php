fcb2f5237ce8ba44033efe8955c163f06645a9e1
Sarah Baldin <baldin@rscw.io>
2023-11-16 18:44:44 +0100

    adjust UserModel to fit calendar app needs

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -12,15 +12,24 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    // a User can have multiple related bookings
+    public function bookings() {
+        return $this->hasMany(Booking::class);
+    }
+
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
         'email',
         'password',
+        'admin',
+        'role',
     ];
 
     /**
