4de1735abc847d5e737c23f2ac728890ebe1ba95
Sarah Baldin <baldin@rscw.io>
2023-11-16 18:43:18 +0100

    adjust migration for users to fit calendar app needs

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->boolean('admin')->default(false);
+            $table->enum('role', ['RS', 'CO', 'EX'])->default('RS');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
