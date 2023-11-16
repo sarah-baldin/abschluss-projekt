3f91af33d2e7c63b952a94413340ca91873b553d
Sarah Baldin <baldin@rscw.io>
2023-11-16 19:38:38 +0100

    add basic api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+/* use Illuminate\Http\Request; */
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/* unprotected routes */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+/*
+    When a request is made to a route that uses the auth:sanctum middleware,
+    Sanctum will check if the request contains a valid API token.
+    If the token is valid, the request is allowed to proceed.
+    If the token is invalid or missing,
+    the request will be rejected with a 401 Unauthorized response.
+*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
 });
