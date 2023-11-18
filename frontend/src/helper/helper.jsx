b0247587835f57d222b0625db0ed5e9615faf612
Sarah Baldin <baldin@rscw.io>
2023-11-18 13:23:39 +0100

    add getBackgroundColorForRoom and transformBookingToEvent functions to helpers

--- frontend/src/helper/helper.jsx
+++ frontend/src/helper/helper.jsx
@@ -1,5 +1,55 @@
 import axios from "../axios";
 
+export const getBackgroundColorForRoom = (roomId) => {
+  const colors = {
+    1: "#1F2041",
+    2: "#4B3F72",
+    3: "#252525",
+    4: "#119DA4",
+    5: "#19647E",
+    6: "#E1A8A4",
+  };
+  return colors[roomId] || "#FFFFFF"; // Fallback-Farbe, falls keine Raum-ID gefunden wurde
+};
+
+export const transformBookingToEvent = (booking) => {
+  const modifyDate = (date) => {
+    return date.replace(" ", "T");
+  };
+
+  const modifiedStartDate = modifyDate(booking.start_date);
+  const modifiedEndDate = modifyDate(booking.end_date);
+
+  return {
+    ...{
+      id: booking.id || null,
+      allday: false,
+      title: booking.customer_name,
+      start: modifiedStartDate,
+      end: modifiedEndDate,
+      backgroundColor: getBackgroundColorForRoom(booking.room_id),
+      extendedProps: {
+        start_date: modifiedStartDate,
+        end_date: modifiedEndDate,
+        person_count: booking.person_count,
+        others: booking.others,
+        user_id: booking.user?.id || 257,
+        room_id: booking.room_id,
+        created_at: booking.created_at || new Date(),
+        updated_at: booking.updated_at || new Date(),
+        user: {
+          firstname: booking.user?.firstname || "Undefined",
+          lastname: booking.user?.lastname || "Test-User",
+          email: booking.user?.email || "undefinded@testuser.de",
+        },
+        materials: [...booking.materials],
+        caterings: [...booking.caterings],
+      },
+    },
+  };
+};
+
+
 export const userIsEventOwner = (user, event) => {
   return event.extendedProps.user_id === user.id;
 };
